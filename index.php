f67d0286d03585af866ab0d1fda2d6f99423cae4
sadru <thesadru@gmail.com>
2020-01-24 10:31:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,18 +13,20 @@
     <body style="background-color: gray;">
         <header>
             <nav>
-                <a href="index.html">main page</a>
-                <a href="secret.html">secret</a>
-                <a href="template.html">template</a>
-                <a href="NGGYU.html">video</a>
-                <a href="containers.html">containers</a>
-                <a href="first.html">initial code</a>
-                <a href="iframe.html">iframes</a>
-                <a href="input.html">inpus</a>
-                <a href="lists.html">lists</a>
-                <a href="metatags.html">metatags test</a>
-                <a href="tables.html">tab;es</a>
-            </nav>
+             <ul>
+                <li><a href="index.html">main page</a></li>
+                <li><a href="secret.html">secret</a></li>
+                <li><a href="template.html">template</a></li>
+                <li><a href="NGGYU.html">video</a></li>
+                <li><a href="containers.html">containers</a></li>
+                <li><a href="first.html">initial code</a></li>
+                <li><a href="iframe.html">iframes</a><li>
+                <li><a href="input.html">inpus</a></li>
+                <li><a href="lists.html">lists</a></li>
+                <li><a href="metatags.html">metatags test</a></li>
+                <li><a href="tables.html">tab;es</a></li>
+             </ul>
+        </nav>
         </header>
 
         <aside>
